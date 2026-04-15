3f8fffabb9929b3677958f9456f3a2ce2544cc51
N-Schwarz <schwarz.nina@aol.de>
2026-04-15 11:42:37 +0200

    logo

--- docs-readthedocs/source/conf.py
+++ docs-readthedocs/source/conf.py
@@ -6,8 +6,11 @@ extensions = [
 ]
 
 html_theme = 'alabaster'
-html_logo = "test-image.jfif"
+html_logo = "_static/cat.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
 }
+
+
+html_static_path = ["_static"]
