79318d54704fd9c74e49533b4e2f4696c63c6c2b
N-Schwarz <schwarz.nina@aol.de>
2026-04-17 15:12:13 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ def main():
     print("Hello, world!")
     print("This is a simple Python program.")
     print("2 + 2 =", 2 + 2)
-
+    print("I changed something")
 if __name__ == "__main__":
     main()
